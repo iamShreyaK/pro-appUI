8c12e90b6c05f34eeb0d36a26b77b78e87130e8a
ShreyaaK <shreyakdev@gmail.com>
2025-12-26 21:44:56 +0530

    final commit

--- js/app.js
+++ js/app.js
@@ -1,17 +1,52 @@
-const tableBody = document.querySelector('#userTable tbody');
 const menuBtn = document.getElementById('menuToggle');
 const sidebar = document.getElementById('sidebar');
-
-users.forEach(user => {
-  const row = document.createElement('tr');
-  row.innerHTML = `
-    <td>${user.name}</td>
-    <td>${user.role}</td>
-    <td>${user.status}</td>
-  `;
-  tableBody.appendChild(row);
-});
 
 menuBtn.addEventListener('click', () => {
   sidebar.classList.toggle('open');
 });
+//modal 
+const modal = document.getElementById('modal');
+
+function openModal(){
+  modal.style.display = 'flex';
+}
+
+function closeModal(){
+  modal.style.display = 'none';
+}
+//rendering data
+
+const users = [
+  {
+    name: 'Alice', role: 'Senior Software Engineer', active: true
+  },
+  {
+    name: 'Jacqeline', role: 'Software Engineer', active: false
+  },
+  {
+    name: 'Jennifer', role: 'Data Analyst', active: false
+  },
+  {
+    name: 'Michael', role: 'Senior Software Engineer', active: true
+  }
+]
+
+const tableBody = document.querySelector("#userTable tbody");
+
+function renderTableData(data){
+   tableBody.innerHTML = '';
+
+  data.forEach(user => {
+    const row = document.createElement('tr');
+    row.innerHTML = `
+                  <td>${user.name}</td>
+                  <td>${user.role}</td>
+                   <div class="status">
+        <span class="status-dot ${user.active ? 'Active' : 'Inactive'}"></span>
+        <span>${user.active ? 'Active' : 'Inactive'}</span>
+        </div>
+                    `;
+    tableBody.appendChild(row);
+  });
+}
+  renderTableData(users)
